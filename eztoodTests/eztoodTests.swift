c1a9f639fa0713068d881878061af8363d3dad2a
klnstprx <ignacy.borzestowski@gmail.com>
2025-05-13 17:49:25 +0900

    Initial Commit

---
+++ eztoodTests/eztoodTests.swift
@@ -0,0 +1,17 @@
+//
+//  eztoodTests.swift
+//  eztoodTests
+//
+//  Created by Ignacy Borzestowski on 13/05/2025.
+//
+
+import Testing
+@testable import eztood
+
+struct eztoodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
